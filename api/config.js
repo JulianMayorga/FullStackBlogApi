109aaec0fcd6281064a0df336c6e425171b61c5e
JulianMayorga <mayorga.julian@gmail.com>
2016-10-16 22:57:38 -0300

    Change process.env.PORT

--- api/config.js
+++ api/config.js
@@ -8,7 +8,7 @@ const environment = {
 }[process.env.NODE_ENV || 'development'];
 
 module.exports = Object.assign({
-  apiHost: process.env.APIHOST || 'localhost',
-  apiPort: process.env.APIPORT,
+  apiHost: process.env.HOST || 'localhost',
+  apiPort: process.env.PORT,
   dbUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017'
 }, environment);
